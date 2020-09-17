a770e2a912b887c699629488c5f9863a7f0a437d
anette12 <70347228+anette12@users.noreply.github.com>
2020-09-17 15:39:20 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    let volumen_piramide = 0
+    basic.showString("volumen piramide=")
+    basic.showNumber(area_base + altura_piramide)
+    basic.showNumber(volumen_piramide / 3)
+})
+let altura_piramide = 0
+let area_base = 0
+basic.showString("Anette Becerra A01285180")
+area_base = randint(1, 100)
+altura_piramide = 4
+basic.forever(function () {
+	
+})
